ccb627cac73d13f72a94bf32ff058158d2d65c5b
Volodymyr Nofenko <700mmr@gmail.com>
2024-05-28 13:12:50 +0300

    raw game done

--- black-jack.py
+++ black-jack.py
@@ -1,5 +1,5 @@
 from random import shuffle
-import os
+import os, time
 
 suits = ["♦", "♣", "♠", "♥"]
 nominals = {"    2": 2, "    3": 3, "    4": 4, "    5": 5, "    6": 6, "    7": 7, "    8": 8, "    9": 9, "   10": 10, " Jack": 10, "Queen": 10, "  King": 10, "  Ace": 11}
@@ -50,12 +50,12 @@ class Player:
         new_player_card = deck.all_cards.pop(0)
         self.current_cards.append(new_player_card)
         self.count_player_points()
-        self.display_player_cards()
 
     def count_player_points (self):
         self.current_points = 0
         for card in self.current_cards:
             self.current_points += card.value
+        self.display_player_cards()
 
     def display_player_cards (self):
         clear()
@@ -74,6 +74,9 @@ class Player:
         for row in rows_to_print:
             print(row)
 
+    def reset_cards (self):
+        self.current_cards = []
+        self.current_points = 0
 
 player = Player(input('Welcome to the Black Jack! Enter your name: '))
 
@@ -84,68 +87,62 @@ while True:
         print('You must select number using 0-9')
     else:
         break
-
-deck = Deck()
-deck.shuffle()
-
-while True: 
-    try:
-        player.place_bet(int(input(f"Please place your bet (from 1 to {player.balance}): ")))
-    except:
-        print('You must select number using 0-9')
-    else:
+while game_on:
+    if player.balance == 0:
+        print('You do not have money :( Come back when you will have some more')
+        game_on = False
         break
-player.hit_another_card()
-
-player_decision = input('Stand or hit? (s / h): ')
-while player_decision == 'h':
+    deck = Deck()
+    deck.shuffle()
+    player.reset_cards()
+    print(f'{player.name}, your current balance is {player.balance}')
+    while True: 
+        try:
+            player.place_bet(int(input(f"Please place your bet (from 1 to {player.balance}): ")))
+        except:
+            print('You must select number using 0-9')
+        else:
+            break
+    
     player.hit_another_card()
-    if player.current_points == 21:
-        print(f'Wow! You won {player.current_bet * 2}!')
-        player.update_money(player.current_bet * 2)
-        break
-        
-    if player.current_points > 21:
-        for card in player.current_cards:
-            if card.value == 11:
-                card.change_ace_value()
-        if player.current_points > 21:
-            print(f'How sad :( You lost your bet of {player.current_bet}')
-            break
-    player_decision = input('Stand or hit? (s / h): ')
-if player_decision == 's':
-    print(f'Got it. You decided to stay with {player.current_points} points. Its dealers turn.')
-    dealer = Player('Dealer')
-    player_condition_to_win = player.current_points > dealer.current_points
-    while player_condition_to_win: 
-        dealer.hit_another_card()
-        if dealer.current_points == 21:
-            print(f'Dealer won.. You lost {player.current_bet}')
-            player.update_money(-player.current_bet)
-            break
 
-        if dealer.current_points > 21:
-            for card in dealer.current_cards:
+    player_decision = input('Stand or hit? (s / h): ')
+    while player_decision == 'h':
+        player.hit_another_card()
+        if player.current_points == 21:
+            print(f'Wow! You won {player.current_bet * 2}!')
+            player.update_money(player.current_bet * 2)
+            break
+            
+        if player.current_points > 21:
+            for card in player.current_cards:
                 if card.value == 11:
                     card.change_ace_value()
-            if dealer.current_points > 21:
-                print(f'Wow! {player.name} won {player.current_bet * 2}!')
+                    player.count_player_points()
+            if player.current_points > 21:
+                print(f'How sad :( You lost your bet of {player.current_bet}')
                 break
-        if dealer.current_points > player.current_points:
-            player.update_money(-player.current_bet)
-            print(f'Dealer has {dealer.current_points} points and {player.name} has {player.current_points} points.. Dealer won, you lost your bet')
-            break
+        player_decision = input('Stand or hit? (s / h): ')
+    if player_decision == 's':
+        print(f'Got it. You decided to stay with {player.current_points} points. Its dealers turn..')
+        time.sleep(3)
+        dealer = Player('Dealer')
+        player_condition_to_win = player.current_points > dealer.current_points
+        while player_condition_to_win: 
+            dealer.hit_another_card()
+            if dealer.current_points == 21:
+                print(f'Dealer won.. You lost {player.current_bet}')
+                break
 
-
-
-# 1) welcome message + 
-# 2) select name and initial balance + 
-# 3) place bet + 
-# 4) show current cards on hand and propose stand or hit +
-# 5) if hit show another card and ask again, +
-#     if player 21 - win round +
-#     if player more than 21 - lose round +
-#     if stand - dealer starts to pick cards
-#     if dealer > player - lose round
-#     if player > dealr - win round
-# 6) start over again
+            if dealer.current_points > 21:
+                for card in dealer.current_cards:
+                    if card.value == 11:
+                        card.change_ace_value()
+                        dealer.count_player_points()
+                if dealer.current_points > 21:
+                    print(f'Wow! {player.name} won {player.current_bet * 2}!')
+                    player.update_money(player.current_bet * 2)
+                    break
+            elif dealer.current_points > player.current_points:
+                print(f'Dealer has {dealer.current_points} points and {player.name} has {player.current_points} points.. Dealer won, you lost your bet')
+                break
